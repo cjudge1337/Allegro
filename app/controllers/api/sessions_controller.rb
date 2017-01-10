3cdc1a29372e2d0b5669427fca30b64352733f76
Carson Judge <cjudge1337@gmail.com>
2017-01-10 13:47:12 -0800

    Create backend auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,30 @@
 class Api::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      sign_in(@user)
+      render '/api/users/show'
+    else
+      render json: ["Invalid username or password"], status: 422
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      sign_out
+      render 'api/users/show'
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
+  end
+
+
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,31 @@
 class Api::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      render :show
+    else
+      render json: @user.errors, response: 422
+    end
+  end
+
+  def show
+    @user = User.new(user_params)
+    if @user
+      render :show
+    else
+      render json: @user.errors, response: 404
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :username)
+  end
+
 end
