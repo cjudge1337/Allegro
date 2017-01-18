3cf43fcbd7d1e9a5538f4ffe1fade7ee4314e0e7
Carson Judge <cjudge1337@gmail.com>
2017-01-18 10:47:50 -0800

    Create user reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import SongsReducer from './song_reducer';
 import NowPlayingReducer from './now_playing_reducer';
+import UserReducer from './user_reducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer,
   songs: SongsReducer,
-  nowPlaying: NowPlayingReducer
+  nowPlaying: NowPlayingReducer,
+  user: UserReducer
 });
 
 export default rootReducer;
---
+++ frontend/reducers/user_reducer.js
@@ -0,0 +1,14 @@
+import { merge } from 'lodash';
+import { RECEIVE_USER } from '../actions/user_actions';
+
+const UserReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_USER:
+      return action.user;
+    default:
+      return state;
+  }
+};
+
+export default UserReducer;
