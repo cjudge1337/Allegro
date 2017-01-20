502a28e79618122603539323fbc707a3a5b61a1a
Carson Judge <cjudge1337@gmail.com>
2017-01-20 15:56:42 -0800

    Fix navbar bug

--- frontend/components/navbar/navbar_container.jsx
+++ frontend/components/navbar/navbar_container.jsx
@@ -3,9 +3,8 @@ import { createSong } from '../../actions/song_actions';
 import { logout } from '../../actions/session_actions';
 import Navbar from './navbar';
 
-const mapStateToProps = ({ session, songs }) => (
-  session,
-  songs
+const mapStateToProps = ({ session }) => (
+  session
 );
 
 const mapDispatchToProps = (dispatch) => ({
